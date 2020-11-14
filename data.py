0a5ac802946831c01f8a5eda24baf45d8ef22f69
David Kim <davidhkim1102@gmail.com>
2020-11-13 21:18:33 -0800

    david's changes

--- data.py
+++ data.py
@@ -1,20 +1,22 @@
 def string():
-    greeting = "Hey, Hey, Hey!"
-    name = "Testing with Strings"
-    doa = "Week 1"
-    job = "Ideating"
-    embed = "https://repl.it/@colinszeto/Testing?lite=true"
+    greeting = "Spaghet"
+    name = "ASCII Chess V. 1"
+    doa = "https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpuTejFE.gif"
+    job = "Our original project, designed to make chess. Note that we did not attempt to reduce the complexity of the code."
+    embed = "https://repl.it/@KyleMyint/ASCII-Chess?lite=true"
     info = {"greeting": greeting, "name": name, "doa": doa, "job": job, "embed": embed}
     return info
 
+
 def billards():
-    greeting = "Hey, Hey, Hey!"
-    name = "Billards Balls"
-    doa = "Week 2"
-    job = "Testing Print with Color"
+    greeting = "Speedrun"
+    name = "Ultimate Tic Tac Toe"
+    doa = "https://lh3.googleusercontent.com/proxy/lfnmXruJn_lshuI73aiV7Jc81SPmkOfHXIYMCX2lAONZguCxXODYgvs18zt3rShLNiDneS_PXKnn"
+    job = "We made this project on a whim, wanting to show something for WOW day. We eventually decided to finish it later."
     embed = "https://repl.it/@colinszeto/Python-Hello-Series?lite=true"
     info = {"greeting": greeting, "name": name, "doa": doa, "job": job, "embed": embed}
     return info
+
 
 def ship():
     greeting = "Hey, Hey, Hey!"
@@ -25,5 +27,6 @@ def ship():
     info = {"greeting": greeting, "name": name, "doa": doa, "job": job, "embed": embed}
     return info
 
+
 def alldata():
-    return [string(), billards(),ship()]
+    return [string(), billards()]
--- main.py
+++ main.py
@@ -1,54 +1,70 @@
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template, request, redirect
 import data
-#create a Flask instance
+
+# create a Flask instance
 app = Flask(__name__)
 
-#connects default URL of server to a python function
+
+# connects default URL of server to a python function
 @app.route('/')
 def home():
-    #function use Flask import (Jinga) to render an HTML template
+    # function use Flask import (Jinga) to render an HTML template
     return render_template("home.html")
+
 
 @app.route('/chessGame/')
 def chessGame():
-    #function use Flask import (Jinga) to render an HTML template
+    # function use Flask import (Jinga) to render an HTML template
     return render_template("chessGame.html")
 
-@app.route("/chessEmbed/")#for the dragable chess file
+
+@app.route("/chessEmbed/")  # for the dragable chess file
 def chessEmbed():
     return render_template("chessEmbed.html")
 
+
 @app.route('/hello/')
 def helloThere():
-    #function use Flask import (Jinga) to render an HTML template
+    # function use Flask import (Jinga) to render an HTML template
     return render_template("helloThere.html")
-#---------------------------------------------------------------------------------------
-@app.route("/yourName") #within the /yourName directory
+
+
+# ---------------------------------------------------------------------------------------
+@app.route("/yourName")  # within the /yourName directory
 def index():
-    return render_template("yourName.html", display="") #this is the default what is displayed (right now it is nothing)
+    return render_template("yourName.html",
+                           display="")  # this is the default what is displayed (right now it is nothing)
 
-@app.route("/printName", methods=['GET','POST']) #this is is where the website directs to when clicking the submit button
+
+@app.route("/printName",
+           methods=['GET', 'POST'])  # this is is where the website directs to when clicking the submit button
 def yourName():
-    if request.method == 'POST': #if the meathod is post
+    if request.method == 'POST':  # if the meathod is post
         form = request.form
-        returnName = str(form['inputName']) #takes the input and converts it to a string, if it was a integer data type be sure to change str to int
-        return render_template("yourName.html", display = returnName) #feeds it back into the template using jinja
-    return redirect("/yourName")#redirects the user back to the page where they entered in the form
-#---------------------------------------------------------------------------------------
+        returnName = str(form['inputName'])
+        # takes the input and converts it to a string, if it was a integer data type be sure to change str to int
+        return render_template("yourName.html", display=returnName)  # feeds it back into the template using jinja
+    return redirect("/yourName")  # redirects the user back to the page where they entered in the form
 
-@app.route("/project/string/")
+
+# ---------------------------------------------------------------------------------------
+
+@app.route("/project/chessv1/")
 def string_route():
     return render_template("task.html", data=data.string())
 
-@app.route("/project/billards/")
+
+@app.route("/project/tictactoe/")
 def billards_route():
     return render_template("task.html", data=data.billards())
+
 
 @app.route("/all/")
 def all_route():
     return render_template("taskall.html", datalist=data.alldata())
 
+
 if __name__ == "__main__":
-    #runs the application on the repl development server
+    # runs the application on the repl development server
     app.run(debug=True)
